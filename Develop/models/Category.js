f56052c7175bc4115284eaf22a8e8ce3dda69a8f
unknown <Jgossett73@gmail.com>
2022-08-12 14:55:08 -0400

    outline build

---
+++ Develop/models/Category.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes, INTEGER, STRING } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id: {
+      type:INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoInrement:true,
+  },
+    name: {
+      type:STRING, 
+      allowNull:false,
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
